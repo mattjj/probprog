397f38c22611eb035d1cc1c7d020dcfd03434904
Matthew Johnson <mattjj@csail.mit.edu>
2013-04-27 11:48:46 -0400

    tests working

--- src/prototype1/test.scm
+++ src/prototype1/test.scm
@@ -7,9 +7,9 @@
 ;; (estimate-indicator-probability dumb2 100)
 ;; correct answer is 1/4. maybe decrease NUM-MH-STEPS.
 (define (dumb2)
-  (let ((x (pramb 0 1))
-        (y (pramb 0 1))
-        (z (pramb 0 1)))
+  (let ((x (discrete '(0 1)))
+        (y (discrete '(0 1)))
+        (z (discrete '(0 1))))
     (let ((sum (+ x y z)))
       (emit (or (= sum 0)
                 (= sum 2))
