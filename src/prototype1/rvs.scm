2d534a95ae645b83a55421d4019068522237c937
Matthew Johnson <mattjj@csail.mit.edu>
2013-05-20 08:56:51 -0400

    simplified files

--- src/prototype1/rvs.scm
+++ src/prototype1/rvs.scm
@@ -1,7 +1,18 @@
 (declare (usual-integrations))
 (load "randutil")
 
-(define (categorical items #!optional weights proposer)
+(define (gaussian mean var #!optional proposer)
+  (if (default-object? proposer)
+    (set! proposer (proposals:additive-gaussian 0 (/ var 4))))
+
+  (let ((params (gaussian:make-params mean var)))
+    (sample
+      (lambda () (gaussian:rvs params))
+      (lambda (rv) (gaussian:log-likelihood rv params))
+      proposer)))
+
+
+(define (discrete items #!optional weights proposer)
   (if (and (default-object? proposer)
            (not (default-object? weights))
            (procedure? weights))
